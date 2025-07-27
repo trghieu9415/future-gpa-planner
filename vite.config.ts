f933a635cddbd16f8daf835d43ff86f12deab7b3
hidenseek9415 <the.first9415@gmail.com>
2025-07-27 15:44:49 +0700

    add allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    allowedHosts: [""],
+    allowedHosts: ["beamish-khapse-3dcafe.netlify.app"],
     host: "::",
     port: 8080,
   },
